358c90447b275a025a59d2d484eee831d00ef58b
BALTIMORE\om.poudel <oppoudel@gmail.com>
2017-07-28 10:27:40 -0400

    Updated Search Page using BooksAPI

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,21 +1,25 @@
+// @ts-nocheck
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import escapeRegExp from 'escape-string-regexp'
-import sortBy from 'sort-by'
 import * as BooksAPI from './BooksAPI'
 
 class SearchPage extends Component {
   state = {
-    query: 'react'
+    query: '',
+    library: []
   }
-  componentDidMount() {
-    BooksAPI.search(this.state.query, 30).then(books => console.log(books))
-  }
+
   handleChange = (event, book) => {
     const shelf = event.target.value
     if (this.props.onChangeShelf) {
       this.props.onChangeShelf(book, shelf)
     }
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    BooksAPI.search(this.state.query, 20).then(books => {
+      this.setState({ library: books })
+    })
   }
   updateQuery = query => {
     this.setState({ query: query.trim() })
@@ -25,16 +29,8 @@ class SearchPage extends Component {
   }
   render() {
     const { books } = this.props
-    const { query } = this.state
+    const { query, library } = this.state
 
-    let displayedBooks
-    if (query) {
-      const match = new RegExp(escapeRegExp(query), 'i')
-      displayedBooks = books.filter(book => match.test(book.title))
-    } else {
-      displayedBooks = books
-    }
-    displayedBooks.sort(sortBy('title'))
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -42,15 +38,7 @@ class SearchPage extends Component {
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <form action="">
+            <form onSubmit={this.handleSubmit}>
               <input
                 type="text"
                 placeholder="Search by title or author"
@@ -62,44 +50,45 @@ class SearchPage extends Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {displayedBooks.map(book =>
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.imageLinks.thumbnail})`
-                      }}
-                    />
-                    <div className="book-shelf-changer">
-                      <select
-                        value={book.shelf}
-                        onChange={e => {
-                          this.handleChange(e, book)
+            {library.length > 0 &&
+              library.map(book =>
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div
+                        className="book-cover"
+                        style={{
+                          width: 128,
+                          height: 193,
+                          backgroundImage: `url(${book.imageLinks.thumbnail})`
                         }}
-                      >
-                        <option value="none" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
+                      />
+                      <div className="book-shelf-changer">
+                        <select
+                          value={book.shelf}
+                          onChange={e => {
+                            this.handleChange(e, book)
+                          }}
+                        >
+                          <option value="none" disabled>
+                            Move to...
+                          </option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">
+                      {book.title}
+                    </div>
+                    <div className="book-authors">
+                      {book.authors.join(', ')}
                     </div>
                   </div>
-                  <div className="book-title">
-                    {book.title}
-                  </div>
-                  <div className="book-authors">
-                    {book.authors.join(', ')}
-                  </div>
-                </div>
-              </li>
-            )}
+                </li>
+              )}
           </ol>
         </div>
       </div>
